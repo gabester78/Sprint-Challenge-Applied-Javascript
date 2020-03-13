3c5f92a8140a5c7f72ba7c627a5d9c408d886a29
Gabriel Romero <gabester78@gmail.com>
2020-03-13 11:15:35 -0500

    Working through tabs.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,39 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// axios
+//   .get("https://lambda-times-backend.herokuapp.com/topics")
+//   .then(response => {
+//     console.log(response);
+//   });
+
+// const subjects = ["Javascript", "BootStrap", "Technology", "Jquery", "Node.js"];
+
+function tab(data) {
+  const div = document.createElement("div");
+  div.classList.add("tab");
+  div.textContent = `${data.topics}`;
+  topics.append(div);
+
+  return div;
+}
+const topics = document.querySelector(".topics");
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+    response.data.message.forEach(item => {
+      topics.append(tab(item));
+    });
+  });
+
+// subjects.map(topics => {
+//   axios
+//     .get("https://lambda-times-backend.herokuapp.com/topics" + topics)
+//     .then(obj => tab(obj.data));
+// });
+
+// axios
+//   .get("https://lambda-times-backend.herokuapp.com/topics")
+//   .then(obj => tab(obj.data));
